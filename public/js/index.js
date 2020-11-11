6b226dead9974585bd3d52db10c6bc259fd7fdf0
KaiqueFigu1 <kaique.lucena@bandtec.com.br>
2020-11-11 12:36:21 -0300

    Cofigurando rota de cadastro

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ function validation(){
 
     showGif()
 
-    let userData = {
+    const userData = {
         email: document.getElementById('email').value,
         nick: document.getElementById('nick').value,
         password: document.getElementById('password').value
@@ -10,13 +10,12 @@ function validation(){
 
     console.log('Validando dados do usuário')
 
-
-    if(userData.email.search('@') != -1){
+    if(userData.email.search('@') != -1 && userData.email.search('.com') != -1){
         console.log('Email ok')
         if(userData.password.length >= 8){
             console.log('Password ok')
-            
 
+            fetchSingup(userData)
         }else{
             console.log('Invalid password')
         }
@@ -34,4 +33,18 @@ function showGif(){
 function closeGif(){
     let waitGif = document.getElementById('wait')
     waitGif.style.display = 'none'
+}
+
+function fetchSingup(userData){
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }
+
+    fetch('/user/singup', options)
+        .then(r => r.json())
+        .then(res => console.log(res))
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
+const express = require('express')
+const Sequelize = require('sequelize')
+const credentials = require('../db/credentials.json')
+
+const router = express.Router()
+const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+
+const sequelize = new Sequelize(credentials.db, credentials.user, credentials.password, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 router.post('/login', (req, res) => {
     const userData = {
@@ -14,13 +22,16 @@ router.post('/login', (req, res) => {
 })
 
 router.post('/singup', (req, res) => {
-    const userData = {
-        "email" : req.body.email,
-        "nickname" : req.body.nickname,
-        "password" : req.body.password
-    }
-
+    console.log(req.body)
+    let sql = `INSERT INTO usuarios VALUES 
+    (null, '${req.body.nick}', '${req.body.email}', '${req.body.password}', 0, 0);`
     
+    sequelize.query(sql, { type: Sequelize.QueryTypes.INSERT })
+        .then(r => {
+            console.log(r)
+        }).catch(err => {
+            console.error(`Ocorreu um erro ${err.errors[0].message} valor ${err.errors[0].value}`)
+        })
 })
 
 
